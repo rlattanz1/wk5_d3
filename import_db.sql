30efe568e2b7193cee6c620fdf4262345fffe813
Rocco Lattanzio <lukelattanzio@yahoo.com>
2023-04-26 14:40:46 -0700

    finished question_likes table

--- import_db.sql
+++ import_db.sql
@@ -15,25 +15,38 @@ CREATE TABLE questions (
 
 -- this is a join table
 CREATE TABLE question_follows (
-    id, INTEGER PRIMARY KEY
+    id INTEGER PRIMARY KEY,
     -- connects the two tables (users and questions) w/ forgn key
-    question_id INTEGER NOT NULL
-    user_id INTEGER NOT NULL
+    question_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
 
     -- Grab the id  and refrences the other table id
     FOREIGN KEY(question_id) REFERENCES questions(id)
-    FOREIGN KEY(user_id) REFERENCES user(id)
+    FOREIGN KEY(user_id) REFERENCES users(id)
 
 );
 
 
 CREATE TABLE replies (
-    id, INTEGER PRIMARY KEY
+    id INTEGER PRIMARY KEY,
+    body TEXT NOT NULL,
+    subject_question_id INTEGER NOT NULL,
+    parent_reply_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+
+    FOREIGN KEY(subject_question_id) REFERENCES questions(id)
+    FOREIGN KEY(parent_reply_id) REFERENCES replies(id)
+    FOREIGN KEY(user_id) REFERENCES users(id)
 );
 
 
 CREATE TABLE question_likes (
-    id, INTEGER PRIMARY KEY
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    question_id INTEGER NOT NULL,
+
+    FOREIGN KEY(user_id) REFERENCES users(id)
+    FOREIGN KEY(question_id) REFERENCES questions(id)
 );
 
 
